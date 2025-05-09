0036dace3bf9fbcb8d4942a4392edfb4f32a3bd1
welsea <welsea.42@gmail.com>
2025-05-09 18:40:49 +0200

    remove: visitor-timeline, visitor-emoji, visitor-federation

--- packages/frontend/src/components/MkVisitorDashboard.vue
+++ packages/frontend/src/components/MkVisitorDashboard.vue
@@ -4,92 +4,141 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<div v-if="instance" :class="$style.root">
-	<div :class="[$style.main, $style.panel]">
-		<img :src="instance.iconUrl || '/favicon.ico'" alt="" :class="$style.mainIcon"/>
-		<button class="_button _acrylic" :class="$style.mainMenu" @click="showMenu"><i class="ti ti-dots"></i></button>
-		<div :class="$style.mainFg">
-			<h1 :class="$style.mainTitle">
-				<!-- 背景色によってはロゴが見えなくなるのでとりあえず無効に -->
-				<!-- <img class="logo" v-if="instance.logoImageUrl" :src="instance.logoImageUrl"><span v-else class="text">{{ instanceName }}</span> -->
-				<span>{{ instanceName }}</span>
-			</h1>
-			<div :class="$style.mainAbout">
-				<!-- eslint-disable-next-line vue/no-v-html -->
-				<div v-html="instance.description || i18n.ts.headlineMisskey"></div>
+	<div v-if="instance" :class="$style.root">
+		<div :class="[$style.main, $style.panel]">
+			<img
+				:src="instance.iconUrl || '/favicon.ico'"
+				alt=""
+				:class="$style.mainIcon"
+			/>
+			<button
+				class="_button _acrylic"
+				:class="$style.mainMenu"
+				@click="showMenu"
+			>
+				<i class="ti ti-dots"></i>
+			</button>
+			<div :class="$style.mainFg">
+				<h1 :class="$style.mainTitle">
+					<!-- 背景色によってはロゴが見えなくなるのでとりあえず無効に -->
+					<!-- <img class="logo" v-if="instance.logoImageUrl" :src="instance.logoImageUrl"><span v-else class="text">{{ instanceName }}</span> -->
+					<span>{{ instanceName }}</span>
+				</h1>
+				<div :class="$style.mainAbout">
+					<!-- eslint-disable-next-line vue/no-v-html -->
+					<div v-html="instance.description || i18n.ts.headlineMisskey"></div>
+				</div>
+				<div
+					v-if="instance.disableRegistration || instance.federation !== 'all'"
+					:class="$style.mainWarn"
+					class="_gaps_s"
+				>
+					<MkInfo v-if="instance.disableRegistration" warn>{{
+						i18n.ts.invitationRequiredToRegister
+					}}</MkInfo>
+					<MkInfo v-if="instance.federation === 'specified'" warn>{{
+						i18n.ts.federationSpecified
+					}}</MkInfo>
+					<MkInfo v-else-if="instance.federation === 'none'" warn>{{
+						i18n.ts.federationDisabled
+					}}</MkInfo>
+				</div>
+				<div class="_gaps_s" :class="$style.mainActions">
+					<MkButton
+						:class="$style.mainAction"
+						full
+						rounded
+						gradate
+						data-cy-signup
+						style="margin-right: 12px"
+						@click="signup()"
+						>{{ i18n.ts.joinThisServer }}</MkButton
+					>
+					<MkButton
+						:class="$style.mainAction"
+						full
+						rounded
+						link
+						to="https://misskey-hub.net/servers/"
+						>{{ i18n.ts.exploreOtherServers }}</MkButton
+					>
+					<MkButton
+						:class="$style.mainAction"
+						full
+						rounded
+						data-cy-signin
+						@click="signin()"
+						>{{ i18n.ts.login }}</MkButton
+					>
+				</div>
 			</div>
-			<div v-if="instance.disableRegistration || instance.federation !== 'all'" :class="$style.mainWarn" class="_gaps_s">
-				<MkInfo v-if="instance.disableRegistration" warn>{{ i18n.ts.invitationRequiredToRegister }}</MkInfo>
-				<MkInfo v-if="instance.federation === 'specified'" warn>{{ i18n.ts.federationSpecified }}</MkInfo>
-				<MkInfo v-else-if="instance.federation === 'none'" warn>{{ i18n.ts.federationDisabled }}</MkInfo>
+		</div>
+		<div v-if="stats" :class="$style.stats">
+			<div :class="[$style.statsItem, $style.panel]">
+				<div :class="$style.statsItemLabel">{{ i18n.ts.users }}</div>
+				<div :class="$style.statsItemCount">
+					<MkNumber :value="stats.originalUsersCount" />
+				</div>
 			</div>
-			<div class="_gaps_s" :class="$style.mainActions">
-				<MkButton :class="$style.mainAction" full rounded gradate data-cy-signup style="margin-right: 12px;" @click="signup()">{{ i18n.ts.joinThisServer }}</MkButton>
-				<MkButton :class="$style.mainAction" full rounded link to="https://misskey-hub.net/servers/">{{ i18n.ts.exploreOtherServers }}</MkButton>
-				<MkButton :class="$style.mainAction" full rounded data-cy-signin @click="signin()">{{ i18n.ts.login }}</MkButton>
+			<div :class="[$style.statsItem, $style.panel]">
+				<div :class="$style.statsItemLabel">{{ i18n.ts.notes }}</div>
+				<div :class="$style.statsItemCount">
+					<MkNumber :value="stats.originalNotesCount" />
+				</div>
 			</div>
 		</div>
-	</div>
-	<div v-if="stats" :class="$style.stats">
-		<div :class="[$style.statsItem, $style.panel]">
-			<div :class="$style.statsItemLabel">{{ i18n.ts.users }}</div>
-			<div :class="$style.statsItemCount"><MkNumber :value="stats.originalUsersCount"/></div>
-		</div>
-		<div :class="[$style.statsItem, $style.panel]">
-			<div :class="$style.statsItemLabel">{{ i18n.ts.notes }}</div>
-			<div :class="$style.statsItemCount"><MkNumber :value="stats.originalNotesCount"/></div>
+		<div :class="$style.panel">
+			<XActiveUsersChart />
 		</div>
 	</div>
-	<div v-if="instance.policies.ltlAvailable" :class="[$style.tl, $style.panel]">
-		<div :class="$style.tlHeader">{{ i18n.ts.letsLookAtTimeline }}</div>
-		<div :class="$style.tlBody">
-			<MkTimeline src="local"/>
-		</div>
-	</div>
-	<div :class="$style.panel">
-		<XActiveUsersChart/>
-	</div>
-</div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-import * as Misskey from 'misskey-js';
-import XSigninDialog from '@/components/MkSigninDialog.vue';
-import XSignupDialog from '@/components/MkSignupDialog.vue';
-import MkButton from '@/components/MkButton.vue';
-import MkTimeline from '@/components/MkTimeline.vue';
-import MkInfo from '@/components/MkInfo.vue';
-import { instanceName } from '@@/js/config.js';
-import * as os from '@/os.js';
-import { misskeyApi } from '@/utility/misskey-api.js';
-import { i18n } from '@/i18n.js';
-import { instance } from '@/instance.js';
-import MkNumber from '@/components/MkNumber.vue';
-import XActiveUsersChart from '@/components/MkVisitorDashboard.ActiveUsersChart.vue';
-import { openInstanceMenu } from '@/ui/_common_/common.js';
-import type { MenuItem } from '@/types/menu.js';
+import { ref } from "vue";
+import * as Misskey from "misskey-js";
+import XSigninDialog from "@/components/MkSigninDialog.vue";
+import XSignupDialog from "@/components/MkSignupDialog.vue";
+import MkButton from "@/components/MkButton.vue";
+import MkTimeline from "@/components/MkTimeline.vue";
+import MkInfo from "@/components/MkInfo.vue";
+import { instanceName } from "@@/js/config.js";
+import * as os from "@/os.js";
+import { misskeyApi } from "@/utility/misskey-api.js";
+import { i18n } from "@/i18n.js";
+import { instance } from "@/instance.js";
+import MkNumber from "@/components/MkNumber.vue";
+import XActiveUsersChart from "@/components/MkVisitorDashboard.ActiveUsersChart.vue";
+import { openInstanceMenu } from "@/ui/_common_/common.js";
+import type { MenuItem } from "@/types/menu.js";
 
 const stats = ref<Misskey.entities.StatsResponse | null>(null);
 
-misskeyApi('stats', {}).then((res) => {
+misskeyApi("stats", {}).then((res) => {
 	stats.value = res;
 });
 
 function signin() {
-	const { dispose } = os.popup(XSigninDialog, {
-		autoSet: true,
-	}, {
-		closed: () => dispose(),
-	});
+	const { dispose } = os.popup(
+		XSigninDialog,
+		{
+			autoSet: true,
+		},
+		{
+			closed: () => dispose(),
+		},
+	);
 }
 
 function signup() {
-	const { dispose } = os.popup(XSignupDialog, {
-		autoSet: true,
-	}, {
-		closed: () => dispose(),
-	});
+	const { dispose } = os.popup(
+		XSignupDialog,
+		{
+			autoSet: true,
+		},
+		{
+			closed: () => dispose(),
+		},
+	);
 }
 
 function showMenu(ev: MouseEvent) {
@@ -108,7 +157,7 @@ function showMenu(ev: MouseEvent) {
 
 .panel {
 	position: relative;
-	background: var(--MI_THEME-panel);
+	background: rgba(255, 255, 255, 0.64);
 	border-radius: var(--MI-radius);
 	box-shadow: 0 12px 32px rgb(0 0 0 / 25%);
 }
